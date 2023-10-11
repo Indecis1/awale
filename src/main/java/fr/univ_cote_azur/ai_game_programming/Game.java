811ea26e24f5b9c6048c2e785754f57c0e231b3b
Amine Haddou <haddouamine2002@gmail.com>
2023-10-11 07:53:12 +0200

    added capturing method to capture the seed. We also modified sowing* methods to return an int which is the last hole snowed. We added holesSeeds method to know how much seeds does a hole have. I also added some java doc information on capturing and sowing to explain how they work.

--- src/main/java/fr/univ_cote_azur/ai_game_programming/Game.java
+++ src/main/java/fr/univ_cote_azur/ai_game_programming/Game.java
@@ -35,11 +35,12 @@ public class Game {
     public void start_game() {
         Scanner sc = new Scanner(System.in);
         boolean endGame = false;
-        boolean isP1_turn = true;
+        boolean isP1_turn = false;
         String playerPlays = "";
 
         // Loop of the game
         while (!endGame) {
+            isP1_turn = !isP1_turn;
             askPlayerNextPlay(isP1_turn);
             playerPlays = sc.nextLine();
             validateInput(isP1_turn, playerPlays);
@@ -47,8 +48,9 @@ public class Game {
             String seedColor = getSeedColor(playerPlays);
             int holeNumberIndex = getHoleNumber(playerPlays);
 
-            sowing(isP1_turn, holeNumberIndex, seedColor);
-            // TODO : capturing function
+            int lastHoleIndex = sowing(isP1_turn, holeNumberIndex, seedColor);
+            capturing(isP1_turn, lastHoleIndex);
+            // Todo afficher les resultats
         }
     }
 
@@ -86,57 +88,99 @@ public class Game {
         return --holeNumberIndex;
     }
 
-    private void sowing(boolean isP1_turn, int holeNumberIndex, String seedColor) {
-        if (seedColor.equalsIgnoreCase("R")) sowingRedSeeds(holeNumberIndex);
-        else if (seedColor.equalsIgnoreCase("TR")) sowingTransparentRedSeeds(holeNumberIndex);
-        else if (isP1_turn && seedColor.equalsIgnoreCase("TB")) sowingTransparentBlueSeeds(1, holeNumberIndex);
-        else if (!isP1_turn && seedColor.equalsIgnoreCase("TB")) sowingTransparentBlueSeeds(0, holeNumberIndex);
+    /**
+     * The methode sowing simulates the sowing of sades starting from the {@code holeNumberIndex} hole.
+     * Depending on the play chose by the player, we will choose the right method to snow the seed without
+     * violate the game's rules.
+     * @param isP1_turn a {@link Boolean} to determine if it is player 1 turn or not.
+     * @param holeNumberIndex an {@link Integer} representing the hole from wihch we take the seed and from where
+     *                        we start the snowing
+     * @param seedColor a {@link String} that represents the seed's color chose by the player.
+     * @return an {@link Integer} which corresponds to the last sowed hole.
+     */
+    private int sowing(boolean isP1_turn, int holeNumberIndex, String seedColor) {
+        int lastHoleIndex = holeNumberIndex;
+        if (seedColor.equalsIgnoreCase("R")) lastHoleIndex = sowingRedSeeds(holeNumberIndex);
+        else if (seedColor.equalsIgnoreCase("TR")) lastHoleIndex = sowingTransparentRedSeeds(holeNumberIndex);
+        else if (isP1_turn && seedColor.equalsIgnoreCase("TB"))
+            lastHoleIndex = sowingTransparentBlueSeeds(1, holeNumberIndex);
+        else if (!isP1_turn && seedColor.equalsIgnoreCase("TB"))
+            lastHoleIndex = sowingTransparentBlueSeeds(0, holeNumberIndex);
         else if (isP1_turn) sowingBlueSeeds(1, holeNumberIndex);
-        else sowingBlueSeeds(0, holeNumberIndex);
+        else lastHoleIndex = sowingBlueSeeds(0, holeNumberIndex);
+        return lastHoleIndex;
     }
 
-    private void sowingRedSeeds(int holeNumberIndex) {
+    private int sowingRedSeeds(int holeNumberIndex) {
         int numberOfRedSeed = redSeeds[holeNumberIndex];
         redSeeds[holeNumberIndex] = 0;
+        int index = holeNumberIndex;
         for (int i = holeNumberIndex + 1; numberOfRedSeed > 0; i++) {
-            int index = i % NUMBER_OF_HOLES;
+            index = i % NUMBER_OF_HOLES;
             if (index == holeNumberIndex) continue;
             redSeeds[index]++;
             numberOfRedSeed--;
         }
+        return index;
     }
 
-    private void sowingTransparentRedSeeds(int holeNumberIndex) {
+    private int sowingTransparentRedSeeds(int holeNumberIndex) {
         int numberOfTransparentSeed = tranparentSeeds[holeNumberIndex];
         tranparentSeeds[holeNumberIndex] = 0;
+        int index = holeNumberIndex;
         for (int i = holeNumberIndex + 1; numberOfTransparentSeed > 0; i++) {
-            int index = i % NUMBER_OF_HOLES;
+            index = i % NUMBER_OF_HOLES;
             if (index == holeNumberIndex) continue;
             tranparentSeeds[index]++;
             numberOfTransparentSeed--;
         }
+        return index;
     }
 
-    private void sowingTransparentBlueSeeds(int polarityOfholes, int holeNumberIndex) {
+    private int sowingTransparentBlueSeeds(int polarityOfholes, int holeNumberIndex) {
         int numberOfBlueSeed = blueSeeds[holeNumberIndex];
         blueSeeds[holeNumberIndex] = 0;
+        int index = holeNumberIndex;
         for (int i = holeNumberIndex + 1; numberOfBlueSeed > 0; i++) {
-            int index = i % NUMBER_OF_HOLES;
+            index = i % NUMBER_OF_HOLES;
             if (index % 2 != polarityOfholes) continue;
             blueSeeds[index]++;
             numberOfBlueSeed--;
         }
+        return index;
     }
 
-    private void sowingBlueSeeds(int polarityOfholes, int holeNumberIndex) {
+    private int sowingBlueSeeds(int polarityOfholes, int holeNumberIndex) {
         int numberOfBlueSeed = blueSeeds[holeNumberIndex];
         blueSeeds[holeNumberIndex] = 0;
+        int index = holeNumberIndex;
         for (int i = holeNumberIndex + 1; numberOfBlueSeed > 0; i++) {
-            int index = i % NUMBER_OF_HOLES;
+            index = i % NUMBER_OF_HOLES;
             if (index % 2 != polarityOfholes) continue;
             blueSeeds[index]++;
             numberOfBlueSeed--;
         }
+        return index;
+    }
+
+    /**
+     * This methode simulate the capture of the seed after the snowing. Thus, it updates the scores' value.
+     * @param isP1_turns a {@link Boolean} to determine if it is player 1 turn or not.
+     * @param lastHoleIndex an {@link Integer} to determine the last sowed hole's index.
+     */
+    private void capturing(boolean isP1_turns, int lastHoleIndex) {
+        for (int i = lastHoleIndex; true; i--) {
+            int score = holesSeeds(i%NUMBER_OF_HOLES);
+            if (score == 0) break;
+            else if (isP1_turns) scoreP1 += score;
+            else scoreP2 += score;
+        }
+    }
+
+    private int holesSeeds(int index) {
+        if ((redSeeds[index] + blueSeeds[index] + tranparentSeeds[index] == 2)) return 2;
+        else if (redSeeds[index] + blueSeeds[index] + tranparentSeeds[index] == 3) return 3;
+        else return 0;
     }
 
 }
