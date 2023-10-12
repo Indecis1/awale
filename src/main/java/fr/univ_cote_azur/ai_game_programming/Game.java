bbb38d052fa3f84eb9e363100c5644a2dc6fa969
Amine Haddou <haddouamine2002@gmail.com>
2023-10-12 19:36:14 +0200

    Correction of the validateInput method to validate the inputs and add exception handling with try-catch. For validateInput, we have modified the regex patterns to make them clearer and valid. We have also added an IllegalArgumentException exception in sowing if the hole doesn't have seeds of the corresponding color. Modify sowingTransparentBlueSeeds and sowingBlueSeeds methods because their conditions to verify the polarity of a hole were false. It is corrected by now.

--- src/main/java/fr/univ_cote_azur/ai_game_programming/Game.java
+++ src/main/java/fr/univ_cote_azur/ai_game_programming/Game.java
@@ -52,12 +52,24 @@ public class Game {
             isP1_turn = !isP1_turn;
             askPlayerNextPlay(isP1_turn);
             playerPlays = sc.nextLine();
-            validateInput(isP1_turn, playerPlays);
-
             String seedColor = getSeedColor(playerPlays);
             int holeNumberIndex = getHoleNumber(playerPlays);
+            try {
+                validateInput(isP1_turn, holeNumberIndex, seedColor);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
 
-            int lastHoleIndex = sowing(isP1_turn, holeNumberIndex, seedColor);
+
+            System.out.println("seed color :" + seedColor + "\nhole number :" + holeNumberIndex);
+            int lastHoleIndex;
+            try {
+                lastHoleIndex = sowing(isP1_turn, holeNumberIndex, seedColor);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
             capturing(isP1_turn, lastHoleIndex);
             printStateOfHoles();
             endGame = one_Player_Have_More_Then_Forty_Seeds() || isaDraw() || notEnoughSeeds();
@@ -65,19 +77,21 @@ public class Game {
         printResult();
     }
 
-    private void validateInput(Boolean isP1_turns, @org.jetbrains.annotations.NotNull String input) {
-        if (!input.matches("^1[0-6]?T?[R|B]$"))
-            throw new IllegalArgumentException("Impossible play from the last player... He lost.\n " + "Wrong color or Number.");
-        if (isP1_turns && input.matches("^\\d*[02468]([A-B]|[a-b])*"))
-            throw new IllegalArgumentException("Player 1 can't play this moov. He has the even holes.");
-        else if (!isP1_turns && !input.matches("^\\d*[02468]([A-B]|[a-b])*")) {
-            throw new IllegalArgumentException("Player 2 can't play this moov. He has the odd holes.");
-        }
+    private void askPlayerNextPlay(boolean isP1_turns) {
+        if (isP1_turns) System.out.print("Player 1's play is : ");
+        else System.out.print("Player 2's play is : ");
     }
 
-    private void askPlayerNextPlay(boolean isP1_turns) {
-        if (isP1_turns) System.out.print("Player 1's play is :");
-        else System.out.print("Player 2's play is :");
+    private void validateInput(Boolean isP1_turns, int holeNumberIndex, String seedColor) {
+        boolean possibleNumber = 0 <= holeNumberIndex && holeNumberIndex < NUMBER_OF_HOLES;
+        boolean possibleColor = seedColor.equals("R") || seedColor.equals("B") || seedColor.equals("TR") || seedColor.equals("TB");
+        if (!possibleNumber || !possibleColor)
+            throw new IllegalArgumentException("Impossible play from the last player... Impossible color or Number.");
+        else if (isP1_turns && (holeNumberIndex + 1) % 2 == 0)
+            throw new IllegalArgumentException("Player 1 can't play this move. He has the odd holes.");
+        else if (!isP1_turns && (holeNumberIndex + 1) % 2 == 1) {
+            throw new IllegalArgumentException("Player 2 can't play this move. He has the even holes.");
+        }
     }
 
     private String getSeedColor(String J1_plays) {
@@ -93,95 +107,110 @@ public class Game {
         int holeNumberIndex = 0;
         for (int i = 0; i < J1_plays.length(); i++) {
             char c = J1_plays.charAt(i);
-            if (Character.isDigit(c)) holeNumberIndex += c;
-            else break;
+            if (Character.isDigit(c)) {
+                holeNumberIndex = holeNumberIndex * 10 + Character.getNumericValue(c);
+            } else {
+                break;
+            }
         }
-        return --holeNumberIndex;
+        return holeNumberIndex - 1;
     }
 
     /**
      * The methode sowing simulates the sowing of sades starting from the {@code holeNumberIndex} hole.
      * Depending on the play chose by the player, we will choose the right method to snow the seed without
      * violate the game's rules.
-     * @param isP1_turn a {@link Boolean} to determine if it is player 1 turn or not.
+     *
+     * @param isP1_turn       a {@link Boolean} to determine if it is player 1 turn or not.
      * @param holeNumberIndex an {@link Integer} representing the hole from wihch we take the seed and from where
      *                        we start the snowing
-     * @param seedColor a {@link String} that represents the seed's color chose by the player.
+     * @param seedColor       a {@link String} that represents the seed's color chose by the player.
      * @return an {@link Integer} which corresponds to the last sowed hole.
      */
     private int sowing(boolean isP1_turn, int holeNumberIndex, String seedColor) {
-        int lastHoleIndex = holeNumberIndex;
-        if (seedColor.equalsIgnoreCase("R")) lastHoleIndex = sowingRedSeeds(holeNumberIndex);
-        else if (seedColor.equalsIgnoreCase("TR")) lastHoleIndex = sowingTransparentRedSeeds(holeNumberIndex);
-        else if (isP1_turn && seedColor.equalsIgnoreCase("TB"))
-            lastHoleIndex = sowingTransparentBlueSeeds(1, holeNumberIndex);
-        else if (!isP1_turn && seedColor.equalsIgnoreCase("TB"))
-            lastHoleIndex = sowingTransparentBlueSeeds(0, holeNumberIndex);
-        else if (isP1_turn) sowingBlueSeeds(1, holeNumberIndex);
+        if (noSeedOfThisColor(holeNumberIndex, seedColor))
+            throw new IllegalArgumentException("There is no " + seedColor + " seed in hole " + (holeNumberIndex + 1));
+        int lastHoleIndex;
+        if (seedColor.equals("R")) lastHoleIndex = sowingRedSeeds(holeNumberIndex);
+        else if (seedColor.equals("TR")) lastHoleIndex = sowingTransparentRedSeeds(holeNumberIndex);
+        else if (isP1_turn && seedColor.equals("TB")) lastHoleIndex = sowingTransparentBlueSeeds(1, holeNumberIndex);
+        else if (!isP1_turn && seedColor.equals("TB")) lastHoleIndex = sowingTransparentBlueSeeds(0, holeNumberIndex);
+        else if (isP1_turn) lastHoleIndex = sowingBlueSeeds(1, holeNumberIndex);
         else lastHoleIndex = sowingBlueSeeds(0, holeNumberIndex);
         return lastHoleIndex;
     }
 
+    private boolean noSeedOfThisColor(int holeNumberIndex, String seedColor) {
+        if (seedColor.equals("R")) {
+            return redSeeds[holeNumberIndex] == 0;
+        } else if (seedColor.equals("B")) {
+            return blueSeeds[holeNumberIndex] == 0;
+        } else {
+            return tranparentSeeds[holeNumberIndex] == 0;
+        }
+    }
+
     private int sowingRedSeeds(int holeNumberIndex) {
-        int numberOfRedSeed = redSeeds[holeNumberIndex];
+        int numberOfRedSeeds = redSeeds[holeNumberIndex];
         redSeeds[holeNumberIndex] = 0;
         int index = holeNumberIndex;
-        for (int i = holeNumberIndex + 1; numberOfRedSeed > 0; i++) {
+        for (int i = holeNumberIndex + 1; numberOfRedSeeds > 0; i++) {
             index = i % NUMBER_OF_HOLES;
             if (index == holeNumberIndex) continue;
             redSeeds[index]++;
-            numberOfRedSeed--;
+            numberOfRedSeeds--;
         }
         return index;
     }
 
     private int sowingTransparentRedSeeds(int holeNumberIndex) {
-        int numberOfTransparentSeed = tranparentSeeds[holeNumberIndex];
+        int numberOfTransparentSeeds = tranparentSeeds[holeNumberIndex];
         tranparentSeeds[holeNumberIndex] = 0;
         int index = holeNumberIndex;
-        for (int i = holeNumberIndex + 1; numberOfTransparentSeed > 0; i++) {
+        for (int i = holeNumberIndex + 1; numberOfTransparentSeeds > 0; i++) {
             index = i % NUMBER_OF_HOLES;
             if (index == holeNumberIndex) continue;
             tranparentSeeds[index]++;
-            numberOfTransparentSeed--;
+            numberOfTransparentSeeds--;
         }
         return index;
     }
 
     private int sowingTransparentBlueSeeds(int polarityOfholes, int holeNumberIndex) {
-        int numberOfBlueSeed = blueSeeds[holeNumberIndex];
-        blueSeeds[holeNumberIndex] = 0;
+        int numberOfTransparentSeeds = tranparentSeeds[holeNumberIndex];
+        tranparentSeeds[holeNumberIndex] = 0;
         int index = holeNumberIndex;
-        for (int i = holeNumberIndex + 1; numberOfBlueSeed > 0; i++) {
+        for (int i = holeNumberIndex + 1; numberOfTransparentSeeds > 0; i++) {
             index = i % NUMBER_OF_HOLES;
-            if (index % 2 != polarityOfholes) continue;
-            blueSeeds[index]++;
-            numberOfBlueSeed--;
+            if (index == holeNumberIndex || (index+1)%2 != polarityOfholes) continue;
+            tranparentSeeds[index]++;
+            numberOfTransparentSeeds--;
         }
         return index;
     }
 
     private int sowingBlueSeeds(int polarityOfholes, int holeNumberIndex) {
-        int numberOfBlueSeed = blueSeeds[holeNumberIndex];
+        int numberOfBlueSeeds = blueSeeds[holeNumberIndex];
         blueSeeds[holeNumberIndex] = 0;
         int index = holeNumberIndex;
-        for (int i = holeNumberIndex + 1; numberOfBlueSeed > 0; i++) {
+        for (int i = holeNumberIndex + 1; numberOfBlueSeeds > 0; i++) {
             index = i % NUMBER_OF_HOLES;
-            if (index % 2 != polarityOfholes) continue;
+            if (index == holeNumberIndex || (index+1)%2 != polarityOfholes) continue;
             blueSeeds[index]++;
-            numberOfBlueSeed--;
+            numberOfBlueSeeds--;
         }
         return index;
     }
 
     /**
      * This methode simulate the capture of the seed after the snowing. Thus, it updates the scores' value.
-     * @param isP1_turns a {@link Boolean} to determine if it is player 1 turn or not.
+     *
+     * @param isP1_turns    a {@link Boolean} to determine if it is player 1 turn or not.
      * @param lastHoleIndex an {@link Integer} to determine the last sowed hole's index.
      */
     private void capturing(boolean isP1_turns, int lastHoleIndex) {
         for (int i = lastHoleIndex; true; i--) {
-            int score = holesSeeds(i%NUMBER_OF_HOLES);
+            int score = holesSeeds(i % NUMBER_OF_HOLES);
             if (score == 0) break;
             else if (isP1_turns) scoreP1 += score;
             else scoreP2 += score;
@@ -194,27 +223,30 @@ public class Game {
         else return 0;
     }
 
-    private void printStateOfHoles(){
-        for (int i = 0; i < NUMBER_OF_HOLES/2; i++) {
-            System.out.print((i+1) + " ( "+ redSeeds[i] +"R, " + blueSeeds[i] +"B, " + tranparentSeeds[i] +"T )");
+    private void printStateOfHoles() {
+        for (int i = 0; i < NUMBER_OF_HOLES / 2; i++) {
+            System.out.print((i + 1) + " ( " + redSeeds[i] + "R, " + blueSeeds[i] + "B, " + tranparentSeeds[i] + "T )  ");
         }
-        for (int i = NUMBER_OF_HOLES-1; i > NUMBER_OF_HOLES/2  ; i++) {
-            System.out.print((i+1) + " ( "+ redSeeds[i] +"R, " + blueSeeds[i] +"B, " + tranparentSeeds[i] +"T )");
+        System.out.println();
+        for (int i = NUMBER_OF_HOLES - 1; i >= NUMBER_OF_HOLES / 2; i--) {
+            System.out.print((i + 1) + " ( " + redSeeds[i] + "R, " + blueSeeds[i] + "B, " + tranparentSeeds[i] + "T )  ");
         }
+        System.out.println();
     }
 
-    private boolean one_Player_Have_More_Then_Forty_Seeds(){
+    private boolean one_Player_Have_More_Then_Forty_Seeds() {
         return scoreP1 > 40 || scoreP2 > 40;
     }
 
-    private boolean isaDraw(){
-        return scoreP1==40 && scoreP2 ==40;
+    private boolean isaDraw() {
+        return scoreP1 == 40 && scoreP2 == 40;
     }
-    private boolean notEnoughSeeds(){
+
+    private boolean notEnoughSeeds() {
         return remainingSeeds() < 10;
     }
 
-    private int remainingSeeds(){
+    private int remainingSeeds() {
         int seedRemained = 0;
         for (int i = 0; i < NUMBER_OF_HOLES; i++) {
             seedRemained += redSeeds[i] + blueSeeds[i] + tranparentSeeds[i];
@@ -222,12 +254,10 @@ public class Game {
         return seedRemained;
     }
 
-    private void printResult(){
-        if (isaDraw())
-            System.out.println("It's a draw (score : " + scoreP1 + " ). Congratulations to both players.");
+    private void printResult() {
+        if (isaDraw()) System.out.println("It's a draw (score : " + scoreP1 + " ). Congratulations to both players.");
         else if (scoreP1 > scoreP2)
             System.out.println("Player 1 has won (score : " + scoreP1 + " ). Congratulations to both players.");
-        else
-            System.out.println("Player 2 has won (score : " + scoreP2 + " ). Congratulations to both players.");
+        else System.out.println("Player 2 has won (score : " + scoreP2 + " ). Congratulations to both players.");
     }
 }
